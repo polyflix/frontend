211d5c3b3e95fefed76b3ff619fc74e2afe58d08
Léo <leodevsecops@gmail.com>
2021-10-05 12:02:55 +0200

    fix: overwrite type

--- src/modules/upload/types/upload.type.ts
+++ src/modules/upload/types/upload.type.ts
@@ -13,6 +13,8 @@ export const BUCKETS = {
   IMAGES: process.env.REACT_APP_MINIO_ENDPOINT_IMAGE || "images",
   SUBTITLES: process.env.REACT_APP_MINIO_ENDPOINT_SUBTITLE || "subtitles",
 };
+
+// eslint-disable-next-line
 export type BUCKETS = typeof BUCKETS[keyof typeof BUCKETS];
 
 export interface IUploadStrategy<T> {
