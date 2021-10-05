62b9b573b89f45e1be0e48592d5210fbfa5cb209
Léo <leodevsecops@gmail.com>
2021-10-05 12:14:40 +0200

    fix: removed comment

--- src/modules/upload/types/upload.type.ts
+++ src/modules/upload/types/upload.type.ts
@@ -2,12 +2,6 @@ export type PresignedUrl = {
   tokenAccess: string;
 };
 
-// export enum Bucket {
-//   IMAGES = "images",
-//   VIDEOS = "videos",
-//   SUBTITLES = "subtitles",
-// }
-
 export const BUCKETS = {
   VIDEOS: process.env.REACT_APP_MINIO_ENDPOINT_VIDEO || "videos",
   IMAGES: process.env.REACT_APP_MINIO_ENDPOINT_IMAGE || "images",
