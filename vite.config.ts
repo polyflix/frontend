20a3bffea88dc78fe140bd0077afa4832b289b46
Thomas Gouveia <thomas.gouveia@etu.umontpellier.fr>
2021-10-12 12:38:55 +0200

    fix: vite config for @mui/icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { find } from 'lodash'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
@@ -8,24 +9,43 @@ import replace from './.vite/replace.plugin'
  * If we are on the development environment,
  */
 const environmentReplacement =
-  (process.env.NODE_ENV || 'local') !== 'local'
+  (process.env.BUILD_MODE || 'local') !== 'local'
     ? [
         {
           file: 'src/environments/environment.ts',
-          replaceWith: `src/environments/${process.env.NODE_ENV}.environment.ts`,
+          replaceWith: `src/environments/${process.env.BUILD_MODE}.environment.ts`,
         },
       ]
     : []
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@theme': resolve(__dirname, './src/theme'),
-      '@env': resolve(__dirname, './src/environments'),
-      '@core': resolve(__dirname, './src/modules/core'),
-      '@auth': resolve(__dirname, './src/modules/authentication'),
-      '@users': resolve(__dirname, './src/modules/users'),
-    },
+    alias: [
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: '@mui/icons-material/esm/$1',
+      },
+      {
+        find: '@theme',
+        replacement: resolve(__dirname, './src/theme'),
+      },
+      {
+        find: '@env',
+        replacement: resolve(__dirname, './src/environments'),
+      },
+      {
+        find: '@core',
+        replacement: resolve(__dirname, './src/modules/core'),
+      },
+      {
+        find: '@auth',
+        replacement: resolve(__dirname, './src/modules/authentication'),
+      },
+      {
+        find: '@users',
+        replacement: resolve(__dirname, './src/modules/users'),
+      },
+    ],
   },
   plugins: [
     react({
