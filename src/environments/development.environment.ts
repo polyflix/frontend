87fc549fe930cd82df625bcb669d88300910eca2
Thomas Gouveia <thomas.gouveia@etu.umontpellier.fr>
2021-10-12 12:47:06 +0200

    chore: update dev configuration

--- src/environments/development.environment.ts
+++ src/environments/development.environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api: 'https://dev.polyflix.dopolytech.fr/api/v1',
+  api: 'https://v2.polyflix.dopolytech.fr/api/v1',
   minioUrl: 'https://minio.dev.polyflix.dopolytech.fr',
 }
