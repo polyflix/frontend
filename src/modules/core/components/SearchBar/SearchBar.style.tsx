2dfcb67524c5833185067433b867b6d7bddb383f
leo.heritier <leo.heritier@etu.umontpellier.fr>
2021-10-15 14:58:26 +0000

    feat(#234): add search bar with CTRL K shortcut

---
+++ src/modules/core/components/SearchBar/SearchBar.style.tsx
@@ -0,0 +1,79 @@
+import TextField from '@mui/material/TextField'
+import { alpha, styled } from '@mui/system'
+
+export const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: theme.palette.grey[200],
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.grey[300], 0.5),
+  },
+  margin: theme.spacing(0, 0, 0, 0),
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(0),
+    width: 'auto',
+  },
+}))
+
+export const SearchIconWrapper = styled('div')(({ theme }) => ({
+  color: theme.palette.grey[500],
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
+
+export const SearchField = styled(TextField)(({ theme }) => ({
+  color: theme.palette.grey[900],
+  '& .MuiInput-root': {
+    borderRadius: theme.shape.borderRadius,
+  },
+  '& fieldset': {
+    border: 'none',
+  },
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    outline: 0,
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '15ch',
+    },
+  },
+}))
+
+export const SearchFieldInModal = styled(TextField)(({ theme }) => ({
+  color: theme.palette.grey[900],
+  '& .MuiInput-root': {
+    borderRadius: theme.shape.borderRadius,
+  },
+  '& fieldset': {
+    border: 'none',
+  },
+  width: '100%',
+
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    outline: 0,
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: '100%',
+  },
+}))
+
+export const boxStyles = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: { xs: 350, md: 550 },
+  bgcolor: 'background.paper',
+  borderRadius: 2,
+  boxShadow: 10,
+  p: 4,
+}
