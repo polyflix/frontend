5719fe8441788a432c1081bf0518c2a2b3d098ed
Hugo Amalric <hugoamalric@gmail.com>
2021-11-03 18:23:56 +0100

    fix(#279): Text transform none for buttons

--- src/theme/typography.config.ts
+++ src/theme/typography.config.ts
@@ -94,6 +94,6 @@ export const typography: TypographyOptions = {
     fontWeight: 700,
     lineHeight: 24 / 14,
     fontSize: pxToRem(14),
-    textTransform: 'capitalize',
+    textTransform: 'none',
   },
 }
