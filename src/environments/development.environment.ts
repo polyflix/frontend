aa9d743ed5a61fb18ac524930d2727f881a571c1
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2021-11-06 12:15:49 +0000

    chore: update development environment to point on dev api instead of v2

--- src/environments/development.environment.ts
+++ src/environments/development.environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@core/types/environment.type'
 
 export const environment: Environment = {
-  api: 'https://v2.polyflix.dopolytech.fr',
+  api: 'https://dev.polyflix.dopolytech.fr',
   minioUrl: 'https://minio.dev.polyflix.dopolytech.fr',
 }
