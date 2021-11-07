4eb5262fe471719e951da5441ab5521b0b9a0ac8
Hugo Amalric <hugoamalric@gmail.com>
2021-11-07 19:24:59 +0100

    fix(videos): fix title overlap

--- src/modules/videos/components/VideoSliderCard/VideoSliderCard.component.tsx
+++ src/modules/videos/components/VideoSliderCard/VideoSliderCard.component.tsx
@@ -17,6 +17,7 @@ import {
   useMediaQuery,
   useTheme,
   Link,
+  Tooltip,
 } from '@mui/material'
 import { abbreviateNumber } from 'js-abbreviation-number'
 import React, { useState } from 'react'
@@ -58,6 +59,9 @@ const VideoSliderOption = () => {
         aria-haspopup="true"
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
+        sx={{
+          width: '100%',
+        }}
       >
         <MoreVertOutlined fontSize="inherit" />
       </IconButton>
@@ -173,20 +177,16 @@ export const VideoSliderCard = ({ video }: Props) => {
                 },
               }}
             />
-
             <Box
               sx={{
                 pl: 1,
-                width: (theme) =>
-                  `calc(100% - ${ltsm ? 30 : 40}px - ${
-                    ltsm ? theme.spacing(1) : theme.spacing(2)
-                  })`,
+                width: '100%',
               }}
             >
               <Box
                 sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
+                  display: 'grid',
+                  gridTemplateColumns: '1fr 28px',
                 }}
               >
                 <Link
@@ -196,21 +196,24 @@ export const VideoSliderCard = ({ video }: Props) => {
                   to={videoSlugLink(video)}
                   sx={{
                     width: '100%',
+                    overflow: 'hidden',
                   }}
                 >
-                  <Typography
-                    fontWeight="bold"
-                    variant="subtitle1"
-                    noWrap={true}
-                    sx={{
-                      fontSize: {
-                        xs: '0.8rem',
-                        md: '1rem',
-                      },
-                    }}
-                  >
-                    {video?.title}
-                  </Typography>
+                  <Tooltip title={video?.title} followCursor>
+                    <Typography
+                      fontWeight="bold"
+                      variant="subtitle1"
+                      noWrap={true}
+                      sx={{
+                        fontSize: {
+                          xs: '0.8rem',
+                          md: '1rem',
+                        },
+                      }}
+                    >
+                      {video?.title}
+                    </Typography>
+                  </Tooltip>
                 </Link>
                 <VideoSliderOption />
               </Box>
