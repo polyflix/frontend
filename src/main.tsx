0b7b8332ed8751b2bfe9a096f7a2ad2dbd352f77
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-06-01 18:50:58 +0200

    feat: add user admin panel

--- src/main.tsx
+++ src/main.tsx
@@ -102,6 +102,11 @@ const PolyflixApp = () => {
               <Route path="/videos" component={VideoRouter} />
               <Route path="/collections" component={CollectionRouter} />
               <Route path="/links" component={LinkRouter} />{' '}
+              <PrivateRoute
+                condition={user?.roles.includes(Role.Admin) || false}
+              >
+                <Route path="/admin" component={AdminRouter} />
+              </PrivateRoute>
               <Route exact path="/" component={HomePage} />
               <PrivateRoute
                 condition={user?.roles.includes(Role.Admin) || false}
