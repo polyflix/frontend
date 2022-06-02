e8466e7c9e7bfbaae26e9d3be09d043e5355adc9
Malo Polese <malo.polese@etu.umontpellier.fr>
2022-06-02 19:22:13 +0200

    chore: update keycloak env

--- src/environments/production.environment.ts
+++ src/environments/production.environment.ts
@@ -3,4 +3,8 @@ import { Environment } from '@core/types/environment.type'
 export const environment: Environment = {
   api: 'https://v2.polyflix.dopolytech.fr',
   minioUrl: 'https://minio.v2.polyflix.dopolytech.fr',
+  urlRealm: 'https://sso.v2.polyflix.dopolytech.fr/',
+  redirectUri: 'https://v2.polyflix.dopolytech.fr/auth/redirect',
+  realm: 'polyflix',
+  clientIdReald: 'polyflix-front',
 }
