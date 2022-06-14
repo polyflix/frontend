efeb67bed5a1ec1bbaba288ace19a0a461b4f2da
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-06-14 12:09:10 +0000

    fix: production environment

--- src/environments/production.environment.ts
+++ src/environments/production.environment.ts
@@ -1,10 +1,10 @@
 import { Environment } from '@core/types/environment.type'
 
 export const environment: Environment = {
-  api: 'https://v2.polyflix.dopolytech.fr',
-  minioUrl: 'https://minio.v2.polyflix.dopolytech.fr',
+  api: 'https://polyflix.dopolytech.fr',
+  minioUrl: 'https://minio.polyflix.dopolytech.fr',
   urlRealm: 'https://sso.polyflix.dopolytech.fr/',
-  redirectUri: 'https://v2.polyflix.dopolytech.fr/auth/redirect',
+  redirectUri: 'https://polyflix.dopolytech.fr/auth/redirect',
   realm: 'polyflix',
   clientIdReald: 'polyflix-front',
 }
