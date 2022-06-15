4aee664a433ee0fecada08f7a21f1a822fe6dea2
sylvain.reynaud <sylvain.reynaud@etu.umontpellier.fr>
2022-06-15 10:54:24 +0200

    feat(certificate): add linkedin link autofill + style

--- src/modules/certifications/pages/Certificate.page.tsx
+++ src/modules/certifications/pages/Certificate.page.tsx
@@ -1,4 +1,13 @@
-import { Typography } from '@mui/material'
+import ShareIcon from '@mui/icons-material/Share'
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material'
 import dayjs from 'dayjs'
 import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
@@ -23,19 +32,88 @@ export const CertificatePage = () => {
         disableGutters={true}
       >
         {certificate ? (
-          <div>
-            <Typography variant="h3">
-              {certificate.certification.name}
-            </Typography>
-            <div>
-              {t('explore.title')}: {id}
-            </div>
-            <div>
-              {t('data.owner')} {certificate.firstName} {certificate.lastName}{' '}
-              {t('data.the')}{' '}
-              {dayjs(certificate.createdAt).format(t('data.dateFormat'))}
-            </div>
-          </div>
+          <Card
+            variant="outlined"
+            sx={{
+              marginX: '9em',
+            }}
+          >
+            <CardContent>
+              <Grid
+                container
+                spacing={2}
+                sx={{
+                  height: '70vh',
+                  width: '100%',
+                  padding: '1em',
+                }}
+              >
+                <Grid item xs={2}>
+                  <Stack
+                    direction="column"
+                    justifyContent="space-between"
+                    alignItems="stretch"
+                  >
+                    <Typography variant="h4">Polyflix University</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={9}>
+                  <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                  >
+                    <div>
+                      <div>
+                        <Typography>{t('data.owner1')}</Typography>
+                        <Typography variant="h2">
+                          {certificate.firstName} {certificate.lastName}{' '}
+                        </Typography>
+                        <Typography>{t('data.owner2')}</Typography>
+                        <Typography variant="h3">
+                          {certificate.certification.name}
+                        </Typography>
+                        <br />
+                        <Typography>
+                          {t('data.date')}{' '}
+                          {dayjs(certificate.createdAt).format(
+                            t('data.dateFormat')
+                          )}
+                        </Typography>
+                      </div>
+                      <br />
+                      <Typography>
+                        {t('explore.title')}: <b>{id}</b>
+                      </Typography>
+                    </div>
+                  </Stack>
+                </Grid>
+              </Grid>
+            </CardContent>
+            <CardActions>
+              <IconButton
+                aria-label="share"
+                onClick={() => {
+                  window.open(
+                    `
+                https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&name=${
+                  certificate.certification.name
+                }&organizationName=Polyflix&issueYear=${dayjs(
+                      certificate.createdAt
+                    ).year()}&issueMonth=${dayjs(
+                      certificate.createdAt
+                    ).month()}&certUrl=https://polyflix.dopolytech.fr/certificate/${id}&certId=${id}
+                `,
+                    '_blank'
+                  )
+                }}
+              >
+                <ShareIcon />
+                <Typography>{t('explore.addLinkedin')}</Typography>
+              </IconButton>
+            </CardActions>
+          </Card>
         ) : (
           <div>{t('empty')}</div>
         )}
