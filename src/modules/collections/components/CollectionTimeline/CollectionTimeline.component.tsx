344551475a08689a8a222db5aec769b5b3200558
JeremyLARDENOIS <jeremy.lardenois@etu.umontpellier.fr>
2022-11-03 15:46:19 +0100

    feat(module): add underline on element on hover

--- src/modules/collections/components/CollectionTimeline/CollectionTimeline.component.tsx
+++ src/modules/collections/components/CollectionTimeline/CollectionTimeline.component.tsx
@@ -1,26 +1,13 @@
-import {
-  Timeline,
-  TimelineItem,
-  TimelineSeparator,
-  TimelineDot,
-  TimelineConnector,
-  TimelineContent,
-} from '@mui/lab'
-import { Typography, Link, Alert, Stack, Skeleton } from '@mui/material'
+import { Timeline } from '@mui/lab'
+import { Alert, Stack, Skeleton } from '@mui/material'
+// import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Link as RouterLink } from 'react-router-dom'
-
-import { Icon } from '@core/components/Icon/Icon.component'
-import { useQuery } from '@core/hooks/useQuery'
-import { Element } from '@core/models/element.model'
-import { ElementType } from '@core/types/element.type'
-import { Role } from '@core/types/roles.type'
-
-import { useAuth } from '@auth/hooks/useAuth.hook'
 
 import { CollectionTimelineSkeleton } from '@collections/components/CollectionTimelineSkeleton/CollectionTimelineSkeleton.component'
 import { Collection } from '@collections/models/collection.model'
 import { useGetCollectionQuery } from '@collections/services/collection.service'
+
+import { ItemCollectionTimeline } from './ItemCollectionTimeline.component'
 
 type CollectionTimelineProps = {
   collectionSlug: string
@@ -34,44 +21,6 @@ export const CollectionTimeline = ({
   })
   const collection: Collection | undefined = data
   const { t } = useTranslation('collections')
-  const query = useQuery() as URLSearchParams
-
-  const { user } = useAuth()
-  const isAdmin = user?.roles?.length && user?.roles?.includes(Role.Admin)
-
-  const elementIcon = (type: ElementType): string => {
-    switch (type) {
-      case 'video':
-        return 'eva:play-circle-outline'
-      case 'link':
-        return 'eva:link-2-fill'
-      case 'quizz':
-        return 'healthicons:i-exam-multiple-choice'
-    }
-  }
-
-  const generateLinkAttribute = (element: Element<any>, index: number) => {
-    switch (element.type) {
-      case 'link':
-        return {
-          href: element.data.url as string,
-          target: '_blank',
-          component: 'a',
-        }
-      case 'quizz':
-        return {
-          href: `/quizzes/${element.id}/play`,
-          target: '_blank',
-          component: 'a',
-        }
-      case 'video':
-        return {
-          to: `/videos/${element.slug}?c=${collection?.slug}&index=${index}`,
-          target: '_self',
-          component: RouterLink,
-        }
-    }
-  }
 
   return (
     <Timeline position="right">
@@ -86,50 +35,13 @@ export const CollectionTimeline = ({
       )}
       {collection?.elements?.map((item, i: number) => {
         return (
-          <Link
+          <ItemCollectionTimeline
             key={i}
-            color="inherit"
-            underline="none"
-            {...generateLinkAttribute(item, i)}
-          >
-            <TimelineItem
-              sx={{
-                '&::before': {
-                  content: 'none',
-                },
-              }}
-            >
-              <TimelineSeparator>
-                <TimelineDot
-                  color={+query.get('index')! === i ? 'primary' : 'grey'}
-                >
-                  <Icon name={elementIcon(item.type)} />
-                </TimelineDot>
-                {i !== collection?.elements?.length - 1 && (
-                  <TimelineConnector />
-                )}
-              </TimelineSeparator>
-              <TimelineContent>
-                <>
-                  {item.visibility === 'private' &&
-                  item?.id != item.user?.id &&
-                  !isAdmin ? (
-                    <Alert severity="error">{t('visibility.private')}</Alert>
-                  ) : (
-                    <>
-                      <Typography variant="body1">{item.name}</Typography>
-                      <Typography
-                        variant="caption"
-                        sx={{ textTransform: 'capitalize' }}
-                      >
-                        {item.type}
-                      </Typography>
-                    </>
-                  )}
-                </>
-              </TimelineContent>
-            </TimelineItem>
-          </Link>
+            index={i}
+            element={item}
+            collectionSlug={collection?.slug}
+            isLast={collection?.elements?.length == i - 1}
+          />
         )
       })}
     </Timeline>
