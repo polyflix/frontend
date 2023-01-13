0788a1a741cf9db33e30978aad19bfdb52b32475
leofvo <leofvo@proton.me>
2023-01-13 16:39:12 +0100

    feat(video): view publisher profile

--- apps/polyflix/src/modules/core/components/CardInformation/card-information.component.tsx
+++ apps/polyflix/src/modules/core/components/CardInformation/card-information.component.tsx
@@ -1,13 +1,18 @@
-import { Stack } from '@mui/material'
+import { Stack, Tooltip, Link } from '@mui/material'
 import { UserAvatar } from '@users/components/UserAvatar/UserAvatar.component'
 import { User } from '@users/models/user.model'
 import dayjs from 'dayjs'
+import { useTranslation } from 'react-i18next'
 import { NullableTypography } from '../NullableTypography/nullable-typography.component'
 import { UserDisplayName } from '../UserDisplayName/user-display-name'
+import { Link as RouterLink } from 'react-router-dom'
 
 type CardInformationProps = {
   user:
-    | (Pick<User, 'firstName'> & Pick<User, 'lastName'> & Pick<User, 'avatar'>)
+    | (Pick<User, 'firstName'> &
+        Pick<User, 'lastName'> &
+        Pick<User, 'avatar'> &
+        Pick<User, 'id'>)
     | undefined
   createdDate: Date | string | undefined
 }
@@ -20,36 +25,47 @@ export const CardInformation = ({
     return dayjs(date).format('DD/MM/YYYY')
   }
 
+  const { t } = useTranslation('users')
+
   return (
-    <Stack
-      direction="row"
-      spacing={1}
-      alignItems="center"
-      sx={{
-        flex: 1,
-        minWidth: 0,
-      }}
-    >
-      <UserAvatar
-        user={user}
-        sx={{
-          width: 25,
-          height: 25,
-        }}
-      />
-      <Stack
-        direction="column"
-        spacing={-0.5}
-        sx={{
-          flex: 1,
-          minWidth: 0,
-        }}
+    <Tooltip title={t<string>('profile.actions.view')}>
+      <Link
+        underline="none"
+        color="inherit"
+        component={RouterLink}
+        to={`/users/${user?.id}/profile/videos`}
       >
-        <UserDisplayName user={user} variant="body2" noWrap />
-        <NullableTypography variant="caption">
-          {displayDate(createdDate)}
-        </NullableTypography>
-      </Stack>
-    </Stack>
+        <Stack
+          direction="row"
+          spacing={1}
+          alignItems="center"
+          sx={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        >
+          <UserAvatar
+            user={user}
+            sx={{
+              width: 25,
+              height: 25,
+            }}
+          />
+          <Stack
+            direction="column"
+            spacing={-0.5}
+            sx={{
+              flex: 1,
+              minWidth: 0,
+            }}
+          >
+            <UserDisplayName user={user} variant="body2" noWrap />
+            <NullableTypography variant="caption">
+              {displayDate(createdDate)}
+            </NullableTypography>
+          </Stack>
+        </Stack>
+      </Link>
+    </Tooltip>
   )
 }
