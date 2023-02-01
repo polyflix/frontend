982fa6e487caa60422a83a12e65951fb0c1fbb5a
Malo Polese <malo.polese@gmail.com>
2023-02-02 00:42:00 +0100

    feat(#121): navbar quick links

--- apps/polyflix/src/modules/studio/hooks/use-pop-over-modal.hook.tsx
+++ apps/polyflix/src/modules/studio/hooks/use-pop-over-modal.hook.tsx
@@ -1,58 +1,61 @@
-import React from "react";
-import { SxProps, Theme, Popover as Modal, styled } from "@mui/material";
-import { PropsWithChildren, useState } from "react";
+import React from 'react'
+import { SxProps, Theme, Popover as Modal, styled } from '@mui/material'
+import { PropsWithChildren, useState } from 'react'
 
 const Popover = styled(Modal)(({ theme }) => ({
-  "& .MuiList-root": {
-    padding: "0",
+  '& .MuiList-root': {
+    padding: '0',
   },
-  "& .MuiButtonBase-root": {
-    padding: "0.2rem 0.5rem",
-    borderRadius: "8px",
+  '& .MuiButtonBase-root': {
+    padding: '0.2rem 0.5rem',
+    borderRadius: '8px',
   },
-}));
+  '& .MuiListItemIcon-root': {
+    minWidth: '40px',
+  },
+}))
 
 type UsePopOverModalReturnProps = {
-  onClick: (event: React.MouseEvent<HTMLButtonElement>, data?: any) => void;
-  outputData: any;
-  handleClose: () => void;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>, data?: any) => void
+  outputData: any
+  handleClose: () => void
   PopOver: ({
     children,
     sx,
-  }: PropsWithChildren<MenuPopoverProps>) => JSX.Element;
-};
+  }: PropsWithChildren<MenuPopoverProps>) => JSX.Element
+}
 
 type MenuPopoverProps = {
-  sx?: SxProps<Theme>;
-};
+  sx?: SxProps<Theme>
+}
 
 export const usePopOverModal = (): UsePopOverModalReturnProps => {
-  const [isOpen, setOpen] = useState(false);
-  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
-  const [outputData, setOutputData] = useState(null);
+  const [isOpen, setOpen] = useState(false)
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null)
+  const [outputData, setOutputData] = useState(null)
 
   const onClick = (event: React.MouseEvent<HTMLButtonElement>, data: any) => {
-    setAnchor(event.currentTarget);
-    setOutputData(data);
-    setOpen(!isOpen);
-  };
+    setAnchor(event.currentTarget)
+    setOutputData(data)
+    setOpen(!isOpen)
+  }
 
   const handleClose = () => {
-    setAnchor(null);
-    setOpen(false);
-  };
+    setAnchor(null)
+    setOpen(false)
+  }
 
   const PopOver = ({ children, sx }: PropsWithChildren<MenuPopoverProps>) => (
     <Popover
-      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       PaperProps={{
         sx: {
-          overflow: "inherit",
+          overflow: 'inherit',
           boxShadow: (theme) => theme.shadows[2],
           border: (theme) => `solid 1px ${theme.palette.background.paper}`,
           background: (theme) => theme.palette.background.paper,
-          padding: "0.3rem",
+          padding: '0.3rem',
           ...sx,
         },
       }}
@@ -62,7 +65,7 @@ export const usePopOverModal = (): UsePopOverModalReturnProps => {
     >
       {children}
     </Popover>
-  );
+  )
 
-  return { PopOver, onClick, handleClose, outputData };
-};
+  return { PopOver, onClick, handleClose, outputData }
+}
